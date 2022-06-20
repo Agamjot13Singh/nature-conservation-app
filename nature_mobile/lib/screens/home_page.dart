9e71fb64749532a1c0553aaec736cc78fbbb3b55
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2022-06-20 23:47:06 +0300

    fixed home_page to be symmetrical. Added buttons.

--- nature_mobile/lib/screens/home_page.dart
+++ nature_mobile/lib/screens/home_page.dart
@@ -12,41 +12,140 @@ class HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        minimum: EdgeInsets.only(left: 20, top: 30),
+        minimum: const EdgeInsets.only(left: 20, right: 20, top: 40),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: const [
-                Icon(
-                  CupertinoIcons.bell_fill,
-                  color: Color(0xfff405B28),
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Icon(CupertinoIcons.tree,
+                    size: 50,
+                    color: Colors.green[800],
+                    shadows: const [
+                      Shadow(
+                        color: Colors.black,
+                        blurRadius: 10,
+                        offset: Offset(0, 1),
+                      ),
+                    ]),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    const Icon(
+                      CupertinoIcons.bell_fill,
+                      color: Color(0xfff405B28),
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    const Icon(
+                      Icons.settings,
+                      color: Color(0xfff405B28),
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    CircleAvatar(
+                      radius: 20,
+                      backgroundImage: AssetImage('assets/images/profile.jpg'),
+                      backgroundColor: Colors.green[900],
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                  ],
                 ),
-                SizedBox(
-                  width: 20,
-                ),
-                Icon(
-                  CupertinoIcons.settings_solid,
-                  color: Color(0xfff405B28),
-                ),
-                SizedBox(
-                  width: 20,
-                )
               ],
             ),
-            Text('Help us save earth',
-                style: GoogleFonts.poppins(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 23,
-                    color: Color(0xfff314222))),
-            SizedBox(
-              height: 40,
+            const SizedBox(height: 20),
+            Text(
+              'How are you, {Name}?',
+              style: GoogleFonts.lato(
+                fontSize: 20,
+                fontWeight: FontWeight.w700,
+                color: Color(0xfff405B28),
+              ),
             ),
-            Text('Actions'),
-            ListView(
-              scrollDirection: Axis.horizontal,
-              children: [],
+            Text(
+              'Help us save earth',
+              style: GoogleFonts.poppins(
+                fontWeight: FontWeight.w600,
+                fontSize: 34,
+                color: Color(0xfff314222),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ButtonBar(
+              mainAxisSize: MainAxisSize.max,
+              alignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Activity',
+                    style: GoogleFonts.lato(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                      color: Color(0xfff314222),
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Statistics',
+                    style: GoogleFonts.lato(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                      color: Color(0xfff314222),
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'My plants',
+                    style: GoogleFonts.lato(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                      color: Color(0xfff314222),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Latest plants",
+                  style: GoogleFonts.roboto(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w800,
+                    color: Color(0xfff314222),
+                  ),
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    const Icon(
+                      CupertinoIcons.square_grid_2x2_fill,
+                      color: Color(0xfff314222),
+                    ),
+                    const Icon(
+                      CupertinoIcons.list_bullet,
+                      color: Color(0xfff314222),
+                    ),
+                  ],
+                )
+              ],
             )
           ],
         ),
